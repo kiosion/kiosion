28cc2638487b92c6842da7c23e6f36a1414b9936
Maxim Dietz <hi@kio.dev>
2024-11-18 17:09:26 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export default {
     githubEvents: 'https://api.github.com/users/kiosion/events',
     wakatime: 'https://wakatime.com/api/v1/users/kiosion/stats',
     music:
-      'https://toru.kio.dev/api/v1/kiosion?blur&border_width=0&border_radius=26',
+      'https://toru.kio.dev/api/v1/kiosion?border_width=0&border_radius=26',
   },
   limits: {
     topRepos: 6,
@@ -20,7 +20,7 @@ export default {
   },
   content: {
     title: `Hi there, I'm Maxim`,
-    description: `I'm a full-stack software developer based out of Toronto working in Fintech. I like building stuff for the web.
+    description: `I'm a full-stack software engineer based out of Toronto working in Cybersec. I like building stuff for the web.
 
 I have a blog where I sometimes write about programming and things I find interesting - [kio.dev](https://kio.dev).
 `
